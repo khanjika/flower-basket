25d9c746a5c99734dbb9c54bf39265bd39799b4f
Maya Varrier <mvarrier@dal.ca>
2020-08-09 19:07:24 -0300

    View update page and update details completed

--- wickerBasket/api/controllers/BasketController.js
+++ wickerBasket/api/controllers/BasketController.js
@@ -63,37 +63,59 @@ module.exports = {
 	updateBasketView: (req, res) => {
 		console.log('Received request to updateBasketView');
 
-		let basketId = req.param('basketId');
-		
-		
-		dbPool.getConnection(function(err, basketDb) {
-			if (err) {
-				console.log('Failed to connect to mysql ' + err);
-				//context.succeed({ statusCode: 500, message: 'Failed to connect to database' });
-				res.send(err);
-				return;
-			}
-			basketDb.query('select * from baskets where basketId = ?', [basketId], (findErr, findResult) => {
-				basketDb.release();
-				if (findErr) {
-					console.log('Error in fetching database ' + findErr);
-					//context.succeed({ statusCode: 500, message: findErr });
-					res.send(findErr);
-					return;
+		request(
+			{
+				url: 'https://t9w66xc78k.execute-api.us-east-1.amazonaws.com/prod/getbasketdetails?basketId=' +req.param('basketId'),
+				method: "GET"
+			},
+			async function(error, response, body) {
+				if (error) {
+					console.log(error);
+					return res.error('Failed to get basket details. Try again');
 				}
+				console.log(response.body);
+				
+				// let response = {
+				// 	statusCode: 200,
+				// 	message: 'Successfull',
+				// 	result: findResult
+				// };
 
-				console.log(findResult);
-				let response = {
-					statusCode: 200,
-					message: 'Successfull',
-					result: findResult
-				};
-				let basket = findResult[0];
-				console.log("Basket name " + basket.basketName);
-				return res.view('pages/updatepage', { basket: findResult[0] });
-				//context.succeed(response);
-			});
-		});
+				let basket = JSON.parse(response.body);
+				console.log(basket);
+				return res.view('pages/updatepage', { basket: basket.result[0] });
+			}
+		);
+		
+		
+		// dbPool.getConnection(function(err, basketDb) {
+		// 	if (err) {
+		// 		console.log('Failed to connect to mysql ' + err);
+		// 		//context.succeed({ statusCode: 500, message: 'Failed to connect to database' });
+		// 		res.send(err);
+		// 		return;
+		// 	}
+		// 	basketDb.query('select * from baskets where basketId = ?', [basketId], (findErr, findResult) => {
+		// 		basketDb.release();
+		// 		if (findErr) {
+		// 			console.log('Error in fetching database ' + findErr);
+		// 			//context.succeed({ statusCode: 500, message: findErr });
+		// 			res.send(findErr);
+		// 			return;
+		// 		}
+
+		// 		console.log(findResult);
+		// 		let response = {
+		// 			statusCode: 200,
+		// 			message: 'Successfull',
+		// 			result: findResult
+		// 		};
+		// 		let basket = findResult[0];
+		// 		console.log("Basket name " + basket.basketName);
+		// 		return res.view('pages/updatepage', { basket: findResult[0] });
+		// 		//context.succeed(response);
+		// 	});
+		// });
 
 
 
@@ -101,64 +123,90 @@ module.exports = {
 
 	updateBasketDetails: (req, res) => {
 
-		console.log(req.body.basketName);
-		let basketName = req.body.basketName;
-		let basketId = req.body.basketId;
-		let details = req.body.details;
-		let price = req.body.price;
-		let quantityAvailable = req.body.quantityAvailable;
+		let bodyData = {
+			basketName : req.body.basketName,
+   			basketId : req.body.basketId,
+   			details : req.body.details,
+   			price : req.body.price,
+   			quantityAvailable : req.body.quantityAvailable
+		  };
+	  
+		request(
+			{
+				url: 'https://d3s7qty54m.execute-api.us-east-1.amazonaws.com/prod/updatebasketdetails',
+				method: "POST",
+        		body: JSON.stringify(bodyData),
 
-		dbPool.getConnection(function (err, basketDb) {
-			if (err) {
-			  console.log("Failed to connect to mysql " + err);
-			  //context.succeed(err);
-			  return;
+			},
+			async function(error, response, body) {
+				if (error) {
+					console.log(error);
+					return res.error('Failed to update basket details. Try again');
+				}
+				console.log(response.body);
+				res.redirect("/");
 			}
-			//console.log(event);
-			basketDb.query(
-			  "select * from baskets where basketId = ?",
-			  [basketId],
-			  (err, result, fields) => {
-				if (err) {
-				  console.log(err);
-				  let response = {
-					statusCode: 500,
-					message: "Error in fetching data from database!!",
-				  };
-				  //context.succeed(response);
-				}
-				if (result.length > 0) {
-				  basketDb.query(
-					"Update baskets set details=?,price=?,quantityAvailable=? where basketId = ?",
-					[details, price, quantityAvailable, basketId],
-					(err, result, fields) => {
-					  if (err) {
-						console.log(err);
-						let response = {
-						  statusCode: 500,
-						  message: "Error in updating data!!",
-						};
-						//context.succeed(response);
-					  }
-					  let response = {
-						statusCode: 200,
-						message: "Successfully updated job details !!",
-					  };
-					  //context.succeed(response);
-					}
-				  );
-				} else {
-				  let response = {
-					statusCode: 203,
-					message: "JobName and partId does not exist !!",
-				  };
-				  //context.succeed(response);
-				}
-			  }
-			);
-			res.redirect("/");
+		);
+
+
+		// console.log(req.body.basketName);
+		// let basketName = req.body.basketName;
+		// let basketId = req.body.basketId;
+		// let details = req.body.details;
+		// let price = req.body.price;
+		// let quantityAvailable = req.body.quantityAvailable;
+
+		// dbPool.getConnection(function (err, basketDb) {
+		// 	if (err) {
+		// 	  console.log("Failed to connect to mysql " + err);
+		// 	  //context.succeed(err);
+		// 	  return;
+		// 	}
+		// 	//console.log(event);
+		// 	basketDb.query(
+		// 	  "select * from baskets where basketId = ?",
+		// 	  [basketId],
+		// 	  (err, result, fields) => {
+		// 		if (err) {
+		// 		  console.log(err);
+		// 		  let response = {
+		// 			statusCode: 500,
+		// 			message: "Error in fetching data from database!!",
+		// 		  };
+		// 		  //context.succeed(response);
+		// 		}
+		// 		if (result.length > 0) {
+		// 		  basketDb.query(
+		// 			"Update baskets set details=?,price=?,quantityAvailable=? where basketId = ?",
+		// 			[details, price, quantityAvailable, basketId],
+		// 			(err, result, fields) => {
+		// 			  if (err) {
+		// 				console.log(err);
+		// 				let response = {
+		// 				  statusCode: 500,
+		// 				  message: "Error in updating data!!",
+		// 				};
+		// 				//context.succeed(response);
+		// 			  }
+		// 			  let response = {
+		// 				statusCode: 200,
+		// 				message: "Successfully updated job details !!",
+		// 			  };
+		// 			  //context.succeed(response);
+		// 			}
+		// 		  );
+		// 		} else {
+		// 		  let response = {
+		// 			statusCode: 203,
+		// 			message: "JobName and partId does not exist !!",
+		// 		  };
+		// 		  //context.succeed(response);
+		// 		}
+		// 	  }
+		// 	);
+		// 	res.redirect("/");
 			
-		  });
+		//   });
 		
 
 	},

ba8020648849d07bae8b37ce1cd823a0daa7d1f2
Maya Varrier <mvarrier@dal.ca>
2020-08-09 00:08:58 -0300

    Update and view of Baskets

--- wickerBasket/api/controllers/BasketController.js
+++ wickerBasket/api/controllers/BasketController.js
@@ -1,3 +1,4 @@
+const request = require('request');
 const mysql = require('mysql');
 
 const dbPool = mysql.createPool({
@@ -13,6 +14,58 @@ module.exports = {
 	getAllBasketListView: (req, res) => {
 
 		console.log('Received request to getAllBasketListView');
+		request.get(
+			{
+				url: 'https://azs5932w5f.execute-api.us-east-1.amazonaws.com/prod/getallbasketlist'
+			},
+			async function(error, response, body) {
+				if (error) {
+					console.log(error);
+					return res.error('Failed to get basket details. Try again');
+				}
+				console.log(response.body);
+
+				let basketList = JSON.parse(response.body);
+				let flag = true;
+				return res.view('pages/homepage', { basketList: basketList.result });
+				
+			}
+		);
+
+
+	},
+
+	getAllBasketList: (req, res) => {
+
+		request.get(
+			{
+				url: 'https://azs5932w5f.execute-api.us-east-1.amazonaws.com/prod/getallbasketlist'
+			},
+			async function(error, response, body) {
+				if (error) {
+					console.log(error);
+					return res.error('Failed to get basket details. Try again');
+				}
+				console.log(response.body);
+
+				let basketList = JSON.parse(response.body);
+				let flag = true;
+				return res.status(200).json(basketList.result);
+			}
+		);
+
+
+	},
+
+
+	addNewBasket: (req, res) => {},
+
+	updateBasketView: (req, res) => {
+		console.log('Received request to updateBasketView');
+
+		let basketId = req.param('basketId');
+		
+		
 		dbPool.getConnection(function(err, basketDb) {
 			if (err) {
 				console.log('Failed to connect to mysql ' + err);
@@ -20,7 +73,7 @@ module.exports = {
 				res.send(err);
 				return;
 			}
-			basketDb.query('select * from baskets', (findErr, findResult) => {
+			basketDb.query('select * from baskets where basketId = ?', [basketId], (findErr, findResult) => {
 				basketDb.release();
 				if (findErr) {
 					console.log('Error in fetching database ' + findErr);
@@ -33,9 +86,11 @@ module.exports = {
 				let response = {
 					statusCode: 200,
 					message: 'Successfull',
-					body: findResult
+					result: findResult
 				};
-				res.send(response);
+				let basket = findResult[0];
+				console.log("Basket name " + basket.basketName);
+				return res.view('pages/updatepage', { basket: findResult[0] });
 				//context.succeed(response);
 			});
 		});
@@ -44,11 +99,69 @@ module.exports = {
 
 	},
 
-	getAllBasketList: (req, res) => {},
+	updateBasketDetails: (req, res) => {
 
-	addNewBasket: (req, res) => {},
+		console.log(req.body.basketName);
+		let basketName = req.body.basketName;
+		let basketId = req.body.basketId;
+		let details = req.body.details;
+		let price = req.body.price;
+		let quantityAvailable = req.body.quantityAvailable;
 
-	updateBasketDetails: (req, res) => {},
+		dbPool.getConnection(function (err, basketDb) {
+			if (err) {
+			  console.log("Failed to connect to mysql " + err);
+			  //context.succeed(err);
+			  return;
+			}
+			//console.log(event);
+			basketDb.query(
+			  "select * from baskets where basketId = ?",
+			  [basketId],
+			  (err, result, fields) => {
+				if (err) {
+				  console.log(err);
+				  let response = {
+					statusCode: 500,
+					message: "Error in fetching data from database!!",
+				  };
+				  //context.succeed(response);
+				}
+				if (result.length > 0) {
+				  basketDb.query(
+					"Update baskets set details=?,price=?,quantityAvailable=? where basketId = ?",
+					[details, price, quantityAvailable, basketId],
+					(err, result, fields) => {
+					  if (err) {
+						console.log(err);
+						let response = {
+						  statusCode: 500,
+						  message: "Error in updating data!!",
+						};
+						//context.succeed(response);
+					  }
+					  let response = {
+						statusCode: 200,
+						message: "Successfully updated job details !!",
+					  };
+					  //context.succeed(response);
+					}
+				  );
+				} else {
+				  let response = {
+					statusCode: 203,
+					message: "JobName and partId does not exist !!",
+				  };
+				  //context.succeed(response);
+				}
+			  }
+			);
+			res.redirect("/");
+			
+		  });
+		
+
+	},
 
 	deleteBasketDetails: (req, res) => {},
 

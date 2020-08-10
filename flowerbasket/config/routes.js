b79535f5c1dba5b46d2857d507691c86b566899c
Ravi Patel <raviom98@gmail.com>
2020-08-10 20:10:25 -0300

    Home page created

--- flowerbasket/config/routes.js
+++ flowerbasket/config/routes.js
@@ -9,35 +9,33 @@
  */
 
 module.exports.routes = {
-	/***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` your home page.            *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
+  /***************************************************************************
+   *                                                                          *
+   * Make the view located at `views/homepage.ejs` your home page.            *
+   *                                                                          *
+   * (Alternatively, remove this and add an `index.html` file in your         *
+   * `assets` directory)                                                      *
+   *                                                                          *
+   ***************************************************************************/
 
-	'/': { view: 'pages/homepage' },
-	'/': { view: 'pages/homepage' },
-	'GET /viewcombos': 'FlowerBasketController.viewCombos',
-	'GET /combo/detail': 'FlowerBasketController.viewComboDetails',
-	'get /login': { view: 'pages/loginpage' },
+  "get /": "FlowerBasketController.viewCombos",
+  "GET /combo/detail": "FlowerBasketController.viewComboDetails",
+  "get /login": { view: "pages/loginpage" },
 
-	'post /login': 'FlowerBasketController.login',
-	'get /signup': 'FlowerBasketController.signUpForm',
+  "post /login": "FlowerBasketController.login",
+  "get /signup": "FlowerBasketController.signUpForm",
 
-	'post /signup': 'FlowerBasketController.signUp',
-	'get /placeOrder': 'FlowerBasketController.placeOrder'
+  "post /signup": "FlowerBasketController.signUp",
+  "get /placeOrder": "FlowerBasketController.placeOrder",
 
-	/***************************************************************************
-  *                                                                          *
-  * More custom routes here...                                               *
-  * (See https://sailsjs.com/config/routes for examples.)                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the routes in this file, it   *
-  * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
-  * not match any of those, it is matched against static assets.             *
-  *                                                                          *
-  ***************************************************************************/
+  /***************************************************************************
+   *                                                                          *
+   * More custom routes here...                                               *
+   * (See https://sailsjs.com/config/routes for examples.)                    *
+   *                                                                          *
+   * If a request to a URL doesn't match any of the routes in this file, it   *
+   * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
+   * not match any of those, it is matched against static assets.             *
+   *                                                                          *
+   ***************************************************************************/
 };
